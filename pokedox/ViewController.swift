e91ca9429039647cd7c262a38e68cbe1d89c268f
Surachet <surachet11@hotmail.com>
2016-11-15 09:33:58 +0700

    Initial Commit

---
+++ pokedox/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokedox
+//
+//  Created by Surachet Songsakaew on 11/15/2559 BE.
+//  Copyright © 2559 Surachet Songsakaew. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
